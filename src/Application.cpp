0b224be6de3a1bd6551debfa38cb0b1d455bb478
Leonor Filipe Fragoso e Santos <up202204354@up.pt>
2024-05-03 12:02:23 +0100

    menu

--- src/Application.cpp
+++ src/Application.cpp
@@ -8,3 +8,111 @@
 #include <iostream>
 
 using namespace std;
+
+Application::Application() {
+    menu();
+}
+
+void Application::menu() {
+    int choice;
+
+    do {
+        cout << endl << "+-----------------------------------------------------------------------------+" << endl;
+        cout << "|  Welcome to the Routing Algorithm for Ocean Shipping and Urban Deliveries!  |" << endl;
+        cout << "|                             We hope to be useful!                           |" << endl;
+        cout << "+-----------------------------------------------------------------------------+" << endl << endl;
+        cout << "1. Load and parse data" << endl;
+        cout << "2. Backtracking Algorithm" << endl;
+        cout << "3. Triangular Approximation Heuristic" << endl;
+        cout << "4. Other Heuristics" << endl;
+        cout << "5. TSP in the Real World" << endl << endl;
+
+        cout << "0. Exit" << endl << endl;
+        cout << "Enter your choice and press ENTER: ";
+        cin >> choice;
+
+        string sel;
+        switch (choice) {
+            case 1:
+                loadData();
+                break;
+            case 2:
+                backtracking();
+                break;
+            case 3:
+                triangular();
+                break;
+            case 4:
+                other();
+                break;
+            case 5:
+                cout << "Enter the **origin** of the Travelling Salesperson Problem: ";
+                cin >> sel;
+                realWorld(sel);
+                break;
+            case 0:
+                cout << "Exiting program. Goodbye!" << endl;
+                break;
+            default:
+                cout << "Invalid choice. Please try again." << endl;
+        }
+    } while (choice != 0);
+}
+
+void Application::loadData() {
+    int choice;
+
+    do {
+        cout << endl;
+        cout << "1. Toy Graph" << endl;
+        cout << "2. Extra Medium-Size Graphs" << endl;
+        cout << "3. Real-world Graphs" << endl << endl;
+
+        cout << "0. Go back" << endl << endl;
+        cout << "Enter your choice and press ENTER: ";
+        cin >> choice;
+
+        string sel;
+        switch (choice) {
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 0:
+                menu();
+            default:
+                cout << "Invalid choice. Please try again." << endl;
+        }
+    } while (choice != 0);
+}
+
+void Application::backtracking() {
+
+    goBack();
+}
+
+void Application::triangular() {
+
+    goBack();
+}
+
+void Application::other() {
+
+    goBack();
+}
+
+void Application::realWorld(string c) {
+    // cout << "Path does not exist! << endl;
+    goBack();
+}
+
+void Application::goBack() {
+    cout << endl << "---------------------------" << endl << "Press ENTER to go back." << endl
+         << "---------------------------" << endl;
+    menu();
+}
--- src/Application.h
+++ src/Application.h
@@ -5,4 +5,22 @@
 #ifndef PROJ2_APPLICATION_H
 #define PROJ2_APPLICATION_H
 
+#include <string>
+#include "Graph.h"
+
+
+class Application {
+
+
+public:
+    Application();
+    void menu();
+    void loadData();
+    void backtracking();
+    void triangular();
+    void other();
+    void realWorld(std::string c);
+    void goBack();
+};
+
 #endif //PROJ2_APPLICATION_H
