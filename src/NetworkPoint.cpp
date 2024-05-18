28c695659f1c1283dc5c9942b547c22d0a2ec637
Leonor Filipe Fragoso e Santos <up202204354@up.pt>
2024-05-18 17:54:52 +0100

    latitude, longitude, goBack, backtracking path, menu

--- src/NetworkPoint.cpp
+++ src/NetworkPoint.cpp
@@ -4,8 +4,22 @@ NetworkPoint::NetworkPoint(unsigned int id) {
     id_ = id;
 }
 
+NetworkPoint::NetworkPoint(unsigned int id, double lat, double lon) {
+    id_ = id;
+    lat_ = lat;
+    lon_ = lon;
+}
+
 unsigned NetworkPoint::getId() const {
     return id_;
+}
+
+double NetworkPoint::getLat() const {
+    return lat_;
+}
+
+double NetworkPoint::getLon() const {
+    return lon_;
 }
 
 bool NetworkPoint::operator==(const NetworkPoint &other) {
